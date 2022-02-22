4b70fa77cfe1ad8c1e588cf409c7afd446d55df0
keraan-1337 <98387188+keraan-1337@users.noreply.github.com>
2022-02-22 13:53:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function mailme() {
     Email.send({
         Host: "smtp.gmail.com",
         Username: "keraan.sadan@younglings.africa",
-        Password: "i_wish_this_was_my_real_password_bro",
+        Password: "try_this_as_my_real_password_please_kind_sir",
         To: email_v,
         From: "keraan.sadan@younglings.africa",
         Subject: "Website: Contact Made!!",
